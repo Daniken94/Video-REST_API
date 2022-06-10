cdf5ced8cf7ba131447a07acbf0e54c36933433b
Kamil Klekotka <kamil.klekotka@gmail.com>
2022-06-10 21:19:24 +0200

    Creating database and update method

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# response = requests.put(BASE + "video/1", {"likes": 10, "name": "Tim", "views": 100})
-# print(response.json())
+response = requests.get(BASE + "video/2")
+print(response.json())
 
-# input()
+input()
 
-response = requests.get(BASE + "video/1", {"likes": 10})
+response = requests.patch(BASE + "video/2", {"views": 98})
 print(response.json())
